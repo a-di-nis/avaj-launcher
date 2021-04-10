47e12f369c04470c06e51551868af00e9224f422
a-di-nis <a-di-nis@student.42.fr>
2021-04-10 23:31:08 +0200

    add exceptions

--- src/fr/ecole/avaj/Exceptions/InvalidArgumentCountException.java
+++ src/fr/ecole/avaj/Exceptions/InvalidArgumentCountException.java
@@ -1,7 +1,7 @@
 package fr.ecole.avaj.Exceptions;
 
 public class InvalidArgumentCountException extends Exception {
-    // static final long serialVersionUID = 1;
+    static final long serialVersionUID = 1;
 
     public InvalidArgumentCountException(String errorMessage) {
         super(errorMessage);
